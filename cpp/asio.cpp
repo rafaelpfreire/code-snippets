88ddf69bee948b98712d0b15daddb02e62451cef
Rafael Freire <rafa.ufabc@gmail.com>
2023-03-14 16:24:55 +0400

    Update asio.cpp

--- cpp/asio.cpp
+++ cpp/asio.cpp
@@ -1,4 +1,4 @@
-#include <boost/asio/io_service.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <chrono>
 #include <iostream>
@@ -7,18 +7,18 @@ using namespace boost::asio;
 
 int main()
 {
-  io_service ioservice;
+  io_context iocontext;
 
-  steady_timer timer1{ioservice, std::chrono::seconds{3}};
+  steady_timer timer1{iocontext, std::chrono::seconds{3}};
   timer1.async_wait([](const boost::system::error_code &ec)
     { std::cout << "3 sec\n"; });
 
-  steady_timer timer2{ioservice, std::chrono::seconds{3}};
+  steady_timer timer2{iocontext, std::chrono::seconds{3}};
   timer2.async_wait([](const boost::system::error_code &ec)
     { std::cout << "3 sec\n"; });
 
   // This will return only when timer1 and timer2 are done
-  ioservice.run();
+  iocontext.run();
 
   return 0;
 }
