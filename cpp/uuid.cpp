10a0382a8053b89bb55320e7690b1d56726c069b
Rafael Freire <rafa.ufabc@gmail.com>
2024-01-15 09:54:53 +0400

    Update uuid.cpp

--- cpp/uuid.cpp
+++ cpp/uuid.cpp
@@ -36,3 +36,25 @@ std::string generate_uuid_v4() {
     return ss.str();
 }
 }  // namespace uuid
+
+
+// This is probably better
+#include <iostream>
+
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+
+
+int main()
+{
+    boost::uuids::random_generator generator;
+
+    boost::uuids::uuid uuid1 = generator();
+    std::cout << uuid1 << std::endl;
+
+    boost::uuids::uuid uuid2 = generator();
+    std::cout << uuid2 << std::endl;
+
+    return 0;
+}
